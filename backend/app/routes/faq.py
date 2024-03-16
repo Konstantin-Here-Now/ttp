b34705defe028c10d6f4a9bff1530af7cf4ed958
Konstantin-Here-Now <riabtsewkonst@gmail.com>
2024-03-17 01:21:40 +0300

    added crud operations for faq

--- backend/app/routes/faq.py
+++ backend/app/routes/faq.py
@@ -1,9 +1,10 @@
 from uuid import UUID
+from sqlalchemy import select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from ..database.database import get_db_session
 from ..database import models as db_models
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, HTTPException, status
 
 from ..models.faq import FAQ
 
@@ -28,15 +29,46 @@ async def create_faq(
 
 
 @router.get("/{id}")
-async def read_faq(id: UUID) -> FAQ:
-    return FAQ
+async def read_faq(
+    id: int,
+    session: AsyncSession = Depends(get_db_session),
+) -> FAQ:
+    faq = await session.get(db_models.FAQ, id)
+    if faq is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Ingredient does not exist",
+        )
+    return faq
+
+
+@router.get("/", status_code=status.HTTP_200_OK)
+async def read_faqs(
+    session: AsyncSession = Depends(get_db_session),
+) -> list[FAQ]:
+    faqs = await session.scalars(select(db_models.FAQ))
+    return faqs
 
 
 @router.put("/{id}")
-async def update_faq(id: UUID) -> None:
+async def update_faq(
+    id: int,
+    data: FAQ,
+    session: AsyncSession = Depends(get_db_session),
+) -> None:
+    faq = await session.get(db_models.FAQ, id)
+    faq.question = data.question
+    faq.answer = data.answer
+    await session.commit()
     return status.HTTP_200_OK
 
 
 @router.delete("/{id}")
-async def delete_faq(id: UUID) -> None:
+async def delete_faq(
+    id: int,
+    session: AsyncSession = Depends(get_db_session),
+) -> None:
+    faq = await session.get(db_models.FAQ, id)
+    await session.delete(faq)
+    await session.commit()
     return status.HTTP_200_OK
