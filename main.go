a588ecdac0f642ea507aae03444905db9871cd3c
Konstantin-Here-Now <tyujas@gmail.com>
2024-05-09 17:45:30 +0300

    Switched to gorilla/mux, added materials getters

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"slices"
 	"strconv"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
 type Day struct {
@@ -23,31 +26,72 @@ type Timetable struct {
 	Sun Day
 }
 
+type Material struct {
+	Id   int
+	Name string
+	Desc string
+	Url  string
+}
+
+var testMaterials []Material
+
 func main() {
 	port := 5432
-	http.HandleFunc("/timetable/", timetableHandler)
+
+	prepareTestData()
+
+	router := mux.NewRouter()
+	router.HandleFunc("/timetable/", getTimetable).Methods("GET")
+	router.HandleFunc("/materials/", getMaterials).Methods("GET")
+	router.HandleFunc("/materials/{id:[0-9]+}", getMaterial).Methods("GET")
 
 	log.Printf("Server stated listening on port %d", port)
 	addr := ":" + strconv.Itoa(port)
-	err := http.ListenAndServe(addr, nil)
-    if err != nil {
-        log.Fatal("ListenAndServe: ", err)
-    }
+	err := http.ListenAndServe(addr, router)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
 
-func timetableHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		getTimetable(w, r)
-	default:
-		http.Error(w, "Invalid HTTP method", http.StatusMethodNotAllowed)
-	}
+func prepareTestData() {
+	testMaterials = append(testMaterials, Material{Id: 1, Name: "Book", Desc: "Interesting"})
+	testMaterials = append(testMaterials, Material{Id: 2, Name: "Paper", Desc: "Wonder"})
+	testMaterials = append(testMaterials, Material{Id: 3, Name: "Video", Desc: ""})
 }
 
 func getTimetable(w http.ResponseWriter, r *http.Request) {
 	testDay := Day{At: "12:00 - 13:00", Date: time.Now()}
 	testTT := Timetable{Mon: testDay, Tue: testDay, Wed: testDay, Thu: testDay, Fri: testDay, Sat: testDay, Sun: testDay}
 	err := json.NewEncoder(w).Encode(testTT)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func getMaterials(w http.ResponseWriter, r *http.Request) {
+	err := json.NewEncoder(w).Encode(testMaterials)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func getMaterial(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, parseErr := strconv.Atoi(vars["id"])
+	if parseErr != nil {
+		panic(parseErr)
+	}
+
+	materialId := slices.IndexFunc(testMaterials, func(m Material) bool { return m.Id == id })
+	if materialId == -1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	material := testMaterials[materialId]
+	err := json.NewEncoder(w).Encode(material)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
